3db5dc82da484e66f31994f1d8e98462fb68fdfc
jiverson002 <jiverson002@csbsju.edu>
2018-02-16 15:44:07 -0600

    separating gem branch (master) and documentation branch (site)

--- lib/canvas-workflow.rb
+++
@@ -1,6 +0,0 @@
-require 'canvas/workflow'
-
-module Canvas
-  module Workflow
-  end
-end
--- lib/canvas/workflow/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Canvas
-  module Workflow
-    VERSION = "0.5.0".freeze
-  end
-end
